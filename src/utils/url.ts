43e973395148b75020643c2350bd0640c81aac1a
Julian Gödde <goedde@tuta.io>
2024-03-12 20:53:03 +0100

    ✨ Add utility method to check for a valid URL

---
+++ src/utils/url.ts
@@ -0,0 +1,17 @@
+/**
+ * @see https://stackoverflow.com/a/5717133
+ * @param str - the string to check against being an url
+ */
+export function isValidHttpUrl(str: string) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i",
+  );
+
+  return pattern.test(str);
+}
